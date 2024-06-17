fe8beed8d79e7bd279037708e14b87db9ae68ba0
lhq01844380@antgroup.com <lhq01844380@antgroup.com>
2024-06-17 10:13:41 +0800

    提交侧边栏

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       {
         docs: {
-          sidebarPath: './sidebar.js',
+          sidebarPath: './sidebars.js',
           // sidebarPath: require.resolve('./sidebar.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
